ec357fc25c39aa4f0c88280b447c8a04f4f7c55d
Laggay-Arthur <39773470+Laggay-Arthur@users.noreply.github.com>
2019-06-08 17:02:44 +0300

    Add files via upload

---
+++ Крестики_нолики_ООП/Крестики_нолики_ООП/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace Крестики_нолики_ООП
+{
+    partial class Form1
+    {
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(3, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(290, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(44, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(223, 32);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Играть";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Image = global::Крестики_нолики_ООП.Properties.Resources.button;
+            this.button2.Location = new System.Drawing.Point(44, 65);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(223, 33);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Выход";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Stencil", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(76, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Главное меню";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(576, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 165);
+            this.panel2.TabIndex = 1;
+            this.panel2.Visible = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(114, 33);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(65, 65);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(9, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(65, 65);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Stencil", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(44, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Выберите кем играть";
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(3, 155);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(517, 285);
+            this.panel3.TabIndex = 2;
+            this.panel3.Visible = false;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.button3);
+            this.panel4.Controls.Add(this.label3);
+            this.panel4.Location = new System.Drawing.Point(618, 302);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(203, 121);
+            this.panel4.TabIndex = 3;
+            this.panel4.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.Image = global::Крестики_нолики_ООП.Properties.Resources.button;
+            this.button3.Location = new System.Drawing.Point(19, 60);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(148, 46);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "В главное меню";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(32, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(857, 43);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(177, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Время: Игра не начата";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(857, 109);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(177, 16);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Сейчас никто не ходит";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1152, 466);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        internal System.Windows.Forms.Panel panel1;
+        internal System.Windows.Forms.Panel panel2;
+        internal System.Windows.Forms.Panel panel3;
+        internal System.Windows.Forms.Panel panel4;
+        internal System.Windows.Forms.Label label3;
+        private System.ComponentModel.IContainer components;
+        private System.Windows.Forms.Label label4;
+        public System.Windows.Forms.Timer timer1;
+        public System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Крестики_нолики_ООП/Крестики_нолики_ООП/Form1.cs
@@ -0,0 +1,431 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Крестики_нолики_ООП
+{
+    public partial class Form1 : Form
+    {
+
+      
+        //Users p1 = new Users();
+        
+        public static int[] GamePoleMap = {
+                             0,0,0,
+                             0,0,0,
+                             0,0,0
+                         };
+        public static int Player = 0, Computer = 0;
+        //поле из 9 картинок
+        public static PictureBox[] GamePole = new PictureBox[9];
+        
+        //переменные для хранение выбора пользователя и игрока кто кем будет играть.
+        //int Player = 0, Computer = 0;
+
+        //игровое поле в виде цифр для просчета выигрыша
+      /*  int[] GamePoleMap = {
+                             0,0,0,
+                             0,0,0,
+                             0,0,0
+                         };*/
+
+        //список имен картинок используемых в игре
+     public  static string[] ImgName =
+        {
+            "pust.jpg", //пустой блок
+           "cres.jpg", //крестик
+            "null.png"
+        };
+        void MainPole()
+        {
+            
+            //задаем начало рисования поля
+            int
+                DX = 0,
+                DY = 0;
+
+            //размеры картинки
+            int
+                HeighP = 95, //высота
+                WhidthP = 100,  //ширина  
+                                //счетчик подсчета картинок
+                IndexPicture = 0;
+            //имя в ячейке будет начинаться с этих символов
+            string NAME = "P_";
+
+            //цикл расстановки ячеек по Y
+            for (int YY = 0; YY < 3; YY++)
+            {
+                //цикл расстановки ячеек по X
+                for (int XX = 0; XX < 3; XX++)
+                {
+                    GamePole[IndexPicture] = new PictureBox()
+                    {
+                        Name = NAME + IndexPicture,                 //Задаем имя картинки
+                        Height = HeighP,                            //задаем размер по Y
+                        Width = WhidthP,                            //задаем размер 
+                        Image = Image.FromFile("pust.jpg"),        //загружаем изображение пустого поля
+                        SizeMode = PictureBoxSizeMode.StretchImage, //заставляем изображение сжаться по размерам картинки
+                        Location = new Point(DX, DY)
+                    };
+
+                    GamePole[IndexPicture].Click += Picture_Click;
+
+                    panel3.Controls.Add(GamePole[IndexPicture]); //размещаем картинку на пенале управления
+                    //рассчитываем новое имя
+                    IndexPicture++;
+
+                    DX += WhidthP; //рассчитываем координаты по X для следующей картинки
+                }
+                DY += HeighP; //рассчитываем координаты по Y для следующей картинки
+                DX = 0; //обнуляем позицию для координаты X
+            }
+        }
+       private void Picture_Click(object sender, EventArgs e)
+        {
+            if (Users.CanStap())
+            {
+                PictureBox ClickImage = sender as PictureBox;
+                string[] ParsName = ClickImage.Name.Split('_');
+
+                int IndexSelectImage = Convert.ToInt32(ParsName[1]);
+
+                GamePole[IndexSelectImage].Image = Image.FromFile(ImgName[Player]);
+                GamePoleMap[IndexSelectImage] = Player;
+
+                if (!Users.TestWin(Player))
+                {
+                    //блокируем поле чтобы игрок не смог ходить
+                    Users.LoockPole();
+                    //Шаг ПК
+                    Users.PC_Step();
+                    //пробуем разблокировать поле 
+                    Users.UnLoockPole();
+                }
+                else
+                {
+                    panel4.Visible = true;
+                    label3.Text = "Вы выиграли";
+                    timer1.Enabled = false;
+                    Users.LoockPole();
+                }
+            }
+        }
+        /*void LoockPole()
+        {
+            //блокируем все поле чтобы игрок не мог на него нажать
+            foreach (PictureBox P in GamePole) P.Enabled = false;
+        }
+        void UnLoockPole()
+        {
+            int Indexx = 0;
+            //разблокируем поля но только те которые не заполнены
+            foreach (PictureBox P in GamePole)
+            {
+                //если поле равно 0 значит есть смысл его открывать
+                if (GamePoleMap[Indexx++] == 0) P.Enabled = true;
+            }
+        }
+      */
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            //создаем поле игрока
+            MainPole();
+           int X3 = panel3.Location.X,
+            X4 = panel4.Location.X,
+            Y3 = panel3.Location.Y,
+            Y4 = panel4.Location.Y;
+            //позиционируем панели на форме
+            panel3.Location = new Point(27, 12);
+            panel4.Location = new Point(600, 12);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Timer = 0;
+            label4.Text = "Игра не начата";
+            //показ выбора Х и О
+            panel2.Visible = true;
+            //прячем панель главного меню
+            panel1.Visible = false;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            //игрок выбрал Х
+            Player = 1;
+            //компьютер играет О
+            Computer = 2;
+            //прячем 2 панель
+            panel2.Visible = false;
+            //показываем поле игры на 3 панели
+            panel3.Visible = true;
+            timer1.Enabled = true;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            //игрок выбрал О
+            Player = 2;
+            //компьютер играет Х
+            Computer = 1;
+            //прячем 2 панель
+            panel2.Visible = false;
+            //показываем поле игры на 3 панели
+            panel3.Visible = true;
+            timer1.Enabled = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //прячем панель 4
+            panel4.Visible = false;
+            //прячем панель 3
+            panel3.Visible = false;
+            //обнуляем карту игры
+            GamePoleMap = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            //обнуляем изображение поля
+            foreach (PictureBox P in GamePole) P.Image = Image.FromFile(ImgName[0]);
+            //обнуляем выбор игрока
+            Player = 0;
+            //обнуляем выбор ПК
+            Computer = 0;
+            //пробуем разблокировать поле 
+            Users.UnLoockPole();
+            //показываем меню игры
+            panel1.Visible = true;
+        }
+
+
+        int Timer=0;
+        public void timer1_Tick(object sender, EventArgs e)
+        {
+            Timer++;
+            
+            label4.Text = "Время игры:" + Timer;
+        }
+        Users us;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        us = new Users(ref label3);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+    public  class Users:Form1
+    {
+
+        //Form1 form = null;
+       public static Label lb;
+
+        //Users pp = new Users(this);
+       
+      
+        public Users(ref Label lb)
+        {
+          
+            this.lb = lb;
+        }
+        public static void LoockPole()
+        {
+         
+            //блокируем все поле чтобы игрок не мог на него нажать
+            foreach (PictureBox P in GamePole) P.Enabled = false;
+        }
+    public static void UnLoockPole()
+        {
+          
+            int Indexx = 0;
+            //разблокируем поля но только те которые не заполнены
+            foreach (PictureBox P in GamePole)
+            {
+                //если поле равно 0 значит есть смысл его открывать
+                if (GamePoleMap[Indexx++] == 0) P.Enabled = true;
+            }
+        }
+   /*  public   int[] GamePoleMap = {
+                             0,0,0,
+                             0,0,0,
+                             0,0,0
+                         };
+        public int Player = 0, Computer = 0;*/
+    public static bool CanStap()
+        {
+           
+            //перебираем все значения игрового поля
+            foreach (int s in GamePoleMap)
+                //если нашли 0 значит есть куда ходить
+                if (s == 0) return true;
+
+            //проверяем не выиграл ли игрок
+            if (TestWin(Player))
+            {
+               
+                lb.Text = "Вы выиграли";
+                LoockPole();
+                //прячем панель игры
+                form.panel4.Visible = true;
+                //если не нашли то ходить больше нельзя
+                return false;
+            }
+            //проверяем не выиграл ли игрок
+            if (TestWin(Computer))
+            {
+               
+                lb.Text = "Вы проиграли";
+                form.panel4.Visible = true;
+                //прячем панель игры
+                LoockPole();
+               form.panel3.Visible = false;
+                return false;
+            }
+
+
+            //если ходить больше нельзя и никто не выиграл значит пишем что ничья
+          
+                lb.Text = "Ничья";
+                //прячем панель игры
+
+                form.panel4.Visible = true;
+                LoockPole();
+            
+
+               
+            
+       
+            return false;
+        }
+        //функция проверки на победу передаем значение кого будем проверять
+     public  static bool TestWin(int WHO)
+        {
+            //список вариантов выигрышных комбинаций
+        int[,] WinVariant =
+            {      {    //1 вариант
+                    1,1,1,  //Х Х Х
+                    0,0,0,  //_ _ _
+                    0,0,0   //_ _ _
+                }, {    //2 вариант
+                    0,0,0,  //_ _ _
+                    1,1,1,  //Х Х Х
+                    0,0,0   //_ _ _
+                }, {    //3 вариант
+                    0,0,0,  //_ _ _
+                    0,0,0,  //_ _ _
+                    1,1,1   //Х Х Х
+                }, {    //4 вариант
+                    1,0,0,  //Х _ _
+                    1,0,0,  //Х _ _
+                    1,0,0   //Х _ _
+                }, {    //5 вариант
+                    0,1,0,  //_ Х _
+                    0,1,0,  //_ Х _
+                    0,1,0   //_ Х _
+                }, {    //6 вариант
+                    0,0,1,  //_ _ Х
+                    0,0,1,  //_ _ Х
+                    0,0,1   //_ _ Х
+                }, {    //7 вариант
+                    1,0,0,  //Х _ _
+                    0,1,0,  //_ Х _
+                    0,0,1   //_ _ Х
+                }, {    //8 вариант
+                    0,0,1,   //_ _ Х
+                    0,1,0,   //_ Х _
+                    1,0,0    //Х _ _
+                }
+            };
+
+            //получаем  поле
+            int[] TestMap = new int[GamePoleMap.Length];
+            //просчитываем поле
+            for (int I = 0; I < GamePoleMap.Length; I++)
+                //если номер в ячейке нам подходит записываем в карту 1
+                if (GamePoleMap[I] == WHO) TestMap[I] = 1;
+
+            //выбираем вариант для сравнения 
+            for (int Variant_Index = 0; Variant_Index < WinVariant.GetLength(0); Variant_Index++)
+            {
+                //счетчик для подсчета соотвествий
+                int WinState = 0;
+                for (int TestIndex = 0; TestIndex < TestMap.Length; TestIndex++)
+                {
+                    //если параметр равен 1 то проверяем его иначе 0 тоже = 0
+                    if (WinVariant[Variant_Index, TestIndex] == 1)
+                    {
+                        //если в параметр  в варианте выигрыша совпал с вариантом на карте считаем это в параметре WinState
+                        if (WinVariant[Variant_Index, TestIndex] == TestMap[TestIndex]) WinState++;
+                    }
+                    //если найдены 3 совпадения значит это и есть выигрышная комбинация
+                    if (WinState == 3) return true;
+                }
+
+            }
+
+            return false;
+
+        }
+      public static void PC_Step()
+        {
+            Form1 form = null;
+            //объявляем функцию генерации случайных чисел 
+            Random Rand = new Random();
+            GENER:
+
+            if (CanStap())
+            {
+                //получаем число от 0 до 8
+                int IndexStep = Rand.Next(0, 8);
+
+                //смотрим если ячейка пуста то ставим туда символ ПК
+                if (GamePoleMap[IndexStep] == 0)
+                {
+                    //рисуем нужную картинку
+                    GamePole[IndexStep].Image = Image.FromFile(ImgName[Computer]);
+                    //записываем в поле игры ход компьютера
+                   GamePoleMap[IndexStep] = Computer;
+
+                }
+                else goto GENER;
+                if (TestWin(Computer))
+                {
+                  
+                        form.panel4.Visible = true;
+                        lb.Text = "Вы Проиграли";
+                        form.timer1.Enabled = false;
+                    
+                 
+                }
+            }
+        }
+
+        
+    }
+
+
+}
